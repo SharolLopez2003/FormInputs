190a8f29d49c1910d45ae3ada537a13878cb3929
Sharol Lopez <21sharol.Lopez@edu.manorisd.net>
2019-01-09 23:35:05 +0000

    changed dropdowns and placeholders

--- flask_app.py
+++ flask_app.py
@@ -13,4 +13,4 @@ def process_inputs():
     select = request.form.get('input_select', '')
     freeform = request.form.get('input_freeform', '')
     return render_template("main_page.html", input_data=dropdown,
-                           output="That' a megga big oof chief %s, let's prey to our lord and savior Pewdiepie to repent our sins." % name)
+                           output="??? Why would you do that? well, that's a megga big oof chief %s, let's prey to our lord and savior, Pewdiepie, to repent our sins." % name)
